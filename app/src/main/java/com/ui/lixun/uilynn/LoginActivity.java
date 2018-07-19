3572c5e7d59583db0e2461c627777320131f1819
p_xxunli <p_xxunli@tencent.com>
2018-07-19 17:34:42 +0800

    all

---
+++ app/src/main/java/com/ui/lixun/uilynn/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.ui.lixun.uilynn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
